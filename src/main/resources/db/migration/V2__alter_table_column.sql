158ec4a141582b0a7de871fa8fc007362516041e
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-18 18:49:26 +0800

    [DannyZeng]SQL Demo Finised

--- src/main/resources/db/migration/V2__alter_table_column.sql
+++ src/main/resources/db/migration/V2__alter_table_column.sql
@@ -1,6 +1,6 @@
-alter table Prosecutor add COLUMN sex varchar(10);
-alter table Prosecutor add COLUMN age int;
+alter table Prosecutor add COLUMN sex check(sex='男'or'女') ;
+alter table Prosecutor add COLUMN age check(age>18);
 alter table Prosecutor add COLUMN birthday varchar(20);
 alter table Prosecutor add COLUMN ID_number varchar(10);
-alter table Prosecutor add COLUMN experience int;
+alter table Prosecutor add COLUMN experience check(experience>0);
 
